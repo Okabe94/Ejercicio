afc0d30eb8a470e893584e8cf5e9335c044acc1a
Okabe94 <33587455+Okabe94@users.noreply.github.com>
2017-11-14 10:44:50 -0500

    Commit a master

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -1,36 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication8;
 
-import java.util.ArrayList;
-import javax.swing.JOptionPane;
 
-/**
- *
- * @author Okabe
- */
 public class JavaApplication8 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-//        String _1 = "A";
-//        String _2= "d";
-//        int res = _1.compareToIgnoreCase(_2);
-//        int absRes=Math.abs(res);
-//        System.out.println(absRes);
-        String[] hola = new String[5];
-        int[] ho = new int[5];
-        hola[0] = "0";
-        hola[1] = "1";
-        hola[2] = "2";
-        int i = cantidadOcupada(hola);
-        
-        System.out.println(i);
+        System.out.println("Hola desde Master");
        
     }
 
